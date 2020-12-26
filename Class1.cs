53c6ee75fb119da29f55f264c5543b2b5e35faf9
ZayatsNikita <pochta,lms@gmail.com>
2020-12-26 19:36:49 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AvlTree
+{
+    class Node<T> where T : IComparable
+    {
+        public int depd;
+        public Node<T> Left { get; protected internal set; }
+        public Node<T> Right { get; protected internal set; }
+        public T Data { get; protected internal set; }
+        public Node(T data)
+        {
+            if(data==null)
+            {
+                throw new NullReferenceException();
+            }
+            Data = data;
+            Left = null;
+            Right = null;
+        }
+    }
+}
+
